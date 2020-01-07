04b8d0ee62b5aa129fdb17e3ca40c47da80c2a97
JIWON <ziwon16@gmail.com>
2020-01-07 20:45:59 +0900

    _8_WILDCARD_DP

--- APSS/_8_WILDCARD.cpp
+++ APSS/_8_WILDCARD.cpp
@@ -2,7 +2,8 @@
 #include <string>
 using namespace std;
 
-/* match: w가 s에 대응 되는지 확인 */
+/* match: w가 s에 대응 되는지 확인 - 단순한 방법 */
+/*
 bool match(const string& w, const string& s) {
 	int pos = 0;
 	while (pos < s.size() && pos < w.size() && (w[pos] == '?' || w[pos] == s[pos]))
@@ -22,19 +23,45 @@ bool match(const string& w, const string& s) {
 	return false;
 
 }
+*/
+ 
+/* 동적계획법 - Memoization */
+int cache[101][101];
+string S, W;
+
+int matchMemoization(int w, int s) {
+	int& ret = cache[w][s];
+	if (ret != -1) return ret;
+
+	while (s < S.size() && w < W.size() && (W[w] == '?' || W[w] == S[s])) {
+		++w;
+		++s;
+	}
+
+	if (w == W.size()) return ret = (s = S.size());
+	if (W[w] == '*')
+		for (int skip = 0; skip + s <= S.size(); ++skip)
+			if (matchMemoization(w + 1, s + skip))
+				return ret = 1;
+
+	return ret = 0;
+
+}
+
 
 int main() {
+	memset(cache, -1, sizeof(cache));
+
 	int c;
 	cin >> c;
 	while (c-- > 0) {
-		string w,s;
 		int n;
-		cin >> w;
+		cin >> W;
 		cin >> n;
 		while (n-- > 0) {
-			cin >> s;
-			if (match(w, s))
-				cout << s<<endl;
+			cin >> S;
+			if (matchMemoization(0,0))
+				cout << S <<endl;
 		}
 	}
 }
