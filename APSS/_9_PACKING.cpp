f37a29b81ec70c1756871ad081c0d6941f1115db
JIWON <ziwon16@gmail.com>
2020-01-10 19:51:53 +0900

    _9_PACKING

---
+++ APSS/_9_PACKING.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int volume[100], need[100];
+int n, capacity;
+int cache[1001][100];
+string name[100];
+
+int pack(int capacity, int item) {
+
+	if (item == n) return 0;
+	int& ret = cache[capacity][item];
+	if (ret != -1) return ret;
+
+	ret = pack(capacity, item + 1);
+	if (capacity >= volume[item])
+		ret = max(ret, pack(capacity - volume[item], item + 1) + need[item]);
+
+	return ret;
+}
