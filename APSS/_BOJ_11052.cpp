e57504a80f41e751db8b9207c20c3bb8a3fc4815
JIWON <ziwon16@gmail.com>
2020-04-18 13:28:39 +0900

    _BOJ_11052_2

--- APSS/_BOJ_11052.cpp
+++ APSS/_BOJ_11052.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 	int n;
 	cin >> n;
-	vector<int> a(n + 1);
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
-	}
 	vector<int> d(n + 1);
+	vector<int> p(n + 1);
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			d[i] = max(d[i], d[i - j] + a[j]);
+		cin >> p[i];
+		d[i] = p[i];
+	}
+	d[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (n - i + j)
+				d[i] = max(d[i], p[i - j] + d[j]);
 		}
 	}
-	cout << d[n] << '\n';
-	return 0;
+	
+	cout << d[n] << "\n";
 }
--- APSS/test.cpp
+++ APSS/test.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	int d[12], t;
-	d[1] = 1, d[2] = 2, d[3] = 4;
-	for (int i = 4; i <= 11; i++) {
-		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+	int n;
+	cin >> n;
+	vector<int> d(n + 1);
+	vector<int> p(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> p[i];
+		d[i] = p[i];
 	}
-
-	cin >> t;
-	while (t-- ) {
-		int n;
-		cin >> n;
-		cout << d[n] << "\n";
+	d[0] = 0;
+	if (n >= 2) {
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j < i; j++) {
+				if (n - i + j)
+					d[i] = max(d[i], p[i - j] + d[j]);
+			}
+		}
 	}
+	cout << d[n] << "\n";
 }
