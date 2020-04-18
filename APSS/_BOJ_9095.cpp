8178afd700c444d68392098993db9dd760f66e1a
JIWON <ziwon16@gmail.com>
2020-04-18 12:38:21 +0900

    _BOJ_9095_2

--- APSS/_BOJ_9095.cpp
+++ APSS/_BOJ_9095.cpp
@@ -21,3 +21,25 @@ int main() {
 		printf("%d\n", d[n]);
 	}
 }
+
+/* ver.2 */
+/*
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int d[12], t;
+	d[1] = 1, d[2] = 2, d[3] = 4;
+	for (int i = 4; i <= 11; i++) {
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+	}
+
+	cin >> t;
+	while (t-- ) {
+		int n;
+		cin >> n;
+		cout << d[n] << "\n";
+	}
+}
+*/
