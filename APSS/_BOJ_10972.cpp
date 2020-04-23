7ae0d5b4aa4af570723625c3ba88d54b8ba5e520
JIWON <ziwon16@gmail.com>
2020-04-23 19:33:29 +0900

    _BOJ_10972

---
+++ APSS/_BOJ_10972.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	vector<int> v(n, 0);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	if (next_permutation(v.begin(), v.end())) {
+		for (int i = 0; i < n; i++) {
+			cout << v[i];
+			if (i != n - 1)
+				cout << " ";
+		}
+		cout << "\n";
+	}
+	else
+		cout << -1 << "\n";
+	return 0;
+}
