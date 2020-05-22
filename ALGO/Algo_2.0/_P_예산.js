3a82170eb2f5308907266b26723f31e66b3ba181
JIWON <ziwon16@gmail.com>
2020-05-22 20:17:27 +0900

    _P_예산

---
+++ ALGO/Algo_2.0/_P_예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+  var answer = 0;
+  d.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < d.length; i++) {
+    if (sum + d[i] > budget) break;
+    sum += d[i];
+    answer++;
+  }
+  return answer;
+}
