2eabc3c924a279e8c44b5512a1166a1d3d1bfb2b
JIWON <ziwon16@gmail.com>
2020-06-08 00:51:44 +0900

    _P_더맵게(수정)

--- APSS/_P_더맵게.cpp
+++ APSS/_P_더맵게.cpp
@@ -1,39 +1,19 @@
-#include <string>
-
 #include <vector>
-
 #include <queue>
-
-
-
 using namespace std;
 
-
-
 int solution(vector<int> scoville, int K) {
-
-	priority_queue<int, vector<int>, greater<int>> PQ;
 	int answer = 0;
-	for (int i = 0; i < scoville.size(); i++) {
-		PQ.push(scoville[i]);
-	}
-
-	int mix;
-	int first, second;
-
-	if (PQ.top() < K) {
-		first = PQ.top();
-		PQ.pop();
-		second = PQ.top();
-		PQ.pop();
+	priority_queue< int, vector<int>, greater<int> > pq;
+	for (int i : scoville)
+		pq.push(i);
+	while (pq.top() < K) {
+		if (pq.size() <= 1) { return -1; }
+		int n = pq.top(); pq.pop();
+		n += (pq.top() * 2);  pq.pop();
+		pq.push(n);
 		answer++;
-		first = first + second * 2;
-		while (PQ.top() < K || first < K) {
-			first = first + PQ.top() * 2;
-			PQ.pop();
-			answer++;
-		}
 	}
-	return answer;
 
+	return answer;
 }
