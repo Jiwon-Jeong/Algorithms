a7e29b4a95d6800910c320a5f79ab7504959d4d7
JIWON <ziwon16@gmail.com>
2020-06-18 00:08:47 +0900

    _leetcode_TIQ_Array_SigleNumber_2

--- APSS/_leetcode_TIQ_Array_SignleNumber.cpp
+++ APSS/_leetcode_TIQ_Array_SignleNumber.cpp
@@ -4,3 +4,19 @@ public:
 		return std::accumulate(nums.begin(), nums.end(), 0, std::bit_xor{});
 	}
 };
+
+// Second Approach
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		while (nums.size() > 1) {
+			if (nums[0] == nums[1]) {
+				nums.erase(nums.begin());
+				nums.erase(nums.begin());
+			}
+			else return nums[0];
+		}
+		return nums.back();
+	}
+};
